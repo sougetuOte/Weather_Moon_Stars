8385a29f689d17a2b3f9aaf9f623ce3f217341ed
sougetuOte <35713865+sougetuOte@users.noreply.github.com>
2023-05-03 07:12:07 +0900

    APIをOpenWeatherMapに変更。月齢計算法変更。月星座データ作成

--- moon_age.py
+++ moon_age.py
@@ -1,9 +1,25 @@
 from datetime import datetime
 import math
 
+def julian_day(date):
+    y = date.year
+    m = date.month
+    d = date.day
+    if m <= 2:
+        y -= 1
+        m += 12
+    A = math.floor(y / 100)
+    B = 2 - A + math.floor(A / 4)
+    return math.floor(365.25 * (y + 4716)) + math.floor(30.6001 * (m + 1)) + d + B - 1524.5
+
+
 def calculate_moon_age():
-    now = datetime.now()
-    new_moon_date = datetime(2000, 1, 6, 18, 14)  # 2000年1月6日18時14分（JST）が新月
-    diff = now - new_moon_date
-    moon_age = (diff.total_seconds() / (24 * 60 * 60)) % 29.53058867  # 月齢を計算
-    return round(moon_age, 2)
+    date = datetime.now()
+    jd = julian_day(date)
+    T = (jd - 2451550.1) / 36525
+    T2 = T * T
+    T3 = T2 * T
+    M = 134.9634 + 477198.8675 * T + 0.0087211 * T2 + T3 / 69699 - T3 / 14712000
+    M = M % 360
+    M = round((M/360)*29.53058867, 2)
+    return M
--- weather_api.py
+++ weather_api.py
@@ -1,24 +1,46 @@
 import requests
-from xml.etree import ElementTree
+from datetime import datetime, timedelta
 
-API_BASE_URL = "https://www.data.jma.go.jp/developer/xml/feed/regular.xml"
+def get_weather_forecast(city_name):
+    api_key = "860b6bf17aa8d789839ca8006b36cd65"
+    base_url = "https://api.openweathermap.org/data/2.5/forecast"
+    params = {
+        'q': city_name,
+        'appid': api_key,
+        'lang': 'ja',
+        'units': 'metric'
+    }
+    response = requests.get(base_url, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        forecast_list = []
+        
+        current_date = datetime.now().date()
+        tomorrow = current_date + timedelta(days=1)
+        
+        for item in data['list']:
+            timestamp = item['dt_txt']
+            date_object = datetime.fromisoformat(timestamp).date()
+            
+            if date_object == current_date:
+                weather_description = item['weather'][0]['description']
+                temperature = item['main']['temp']
 
-def get_weather_forecast(city):
-    try:
-        response = requests.get(API_BASE_URL)
-        response.raise_for_status()
-    except requests.RequestException:
-        return None
+                forecast_list.append(
+                    f"{timestamp}: 天気 - {weather_description}, 気温 - {temperature}℃"
+                )
+                
+            elif date_object >= tomorrow:
+                break
+        
+        return "\n".join(forecast_list)
+    
+    else:
+        return f"エラー: {response.status_code}"
 
-    xml_data = ElementTree.fromstring(response.content)
-
-    # XMLデータから都市の天気情報を取得
-    weather_forecast = ""
-    for area in xml_data.findall(".//area"):
-        if area.find("name").text == city:
-            for time in area.findall(".//time"):
-                weather_forecast += f"{time.find('date').text} {time.find('hour').text}時: {time.find('weather').text}\n"
-
-            break
-
-    return weather_forecast if weather_forecast else None
+if __name__ == "__main__":
+    city_name = input("都市名を日本語またはアルファベットで入力してください: ")
+#    api_key = "860b6bf17aa8d789839ca8006b36cd65"  # OpenWeatherMapのAPIキーを入力してください
+    forecast = get_weather_forecast(city_name)
+    print(forecast)
