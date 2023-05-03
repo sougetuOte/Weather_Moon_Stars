dc78571fcd1c6da1a6edcaef39f93713fdae7b81
sougetuOte <35713865+sougetuOte@users.noreply.github.com>
2023-05-03 21:42:38 +0900

    iniファイル実装に伴う変更

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
-import os
+import configparser
 
-ASTROLOGY_DATA_FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "astrology_data.json")
+def read_config():
+    config = configparser.ConfigParser()
+    config.read("app_config.ini")
+
+    api_key = config.get("OpenWeather", "API_KEY")
+    astrology_data_file = config.get("Astrology", "ASTROLOGY_DATA_FILE")
+
+    return api_key, astrology_data_file
